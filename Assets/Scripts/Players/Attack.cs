707052e739e393bb80ac0a62961fd9a62a5d6d13
it262 <sasura516@yahoo.co.jp>
2019-03-22 01:43:12 +0900

    Itaku20190322

---
+++ Assets/Scripts/Players/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+	[SerializeField]GameObject Arm;
+
+	public float speed = 1f;
+
+	Vector3 buffer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		buffer = Arm.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButton (0)) {
+			if (transform.localPosition.z < Arm.transform.localPosition.z) {
+				Arm.transform.RotateAround (transform.position, Vector3.up, -speed);
+			} else {
+				Arm.transform.localPosition = buffer;
+			}
+			return;
+		}
+		Arm.transform.localPosition = buffer;
+    }
+}
