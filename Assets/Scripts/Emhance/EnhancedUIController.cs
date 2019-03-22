a211a329c50470dfa53a9844ec9fb6c61e7d8e53
narusei <narusei@users.noreply.github.com>
2019-03-23 00:13:01 +0900

    EnhancedUI

---
+++ Assets/Scripts/Emhance/EnhancedUIController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnhancedUIController : MonoBehaviour
+{
+    float attack = 50f;
+    float defence = 50f;
+    float speed = 50f;
+
+    public GameObject AttackUI, DefenceUI, SpeedUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickAttack()
+    {
+        attack += 2f;
+        defence -= 2f;
+        speed -= 1f;
+
+        AttackUI.GetComponent<filledImage>().changeTargetAmount(attack);
+        DefenceUI.GetComponent<filledImage>().changeTargetAmount(defence);
+        SpeedUI.GetComponent<filledImage>().changeTargetAmount(speed);
+    }
+
+    public void ClickDefence()
+    {
+        attack -= 1f;
+        defence += 2f;
+        speed -= 2f;
+
+        AttackUI.GetComponent<filledImage>().changeTargetAmount(attack);
+        DefenceUI.GetComponent<filledImage>().changeTargetAmount(defence);
+        SpeedUI.GetComponent<filledImage>().changeTargetAmount(speed);
+    }
+
+    public void ClickSpeed()
+    {
+        attack -= 2f;
+        defence -= 1f;
+        speed += 2f;
+
+        AttackUI.GetComponent<filledImage>().changeTargetAmount(attack);
+        DefenceUI.GetComponent<filledImage>().changeTargetAmount(defence);
+        SpeedUI.GetComponent<filledImage>().changeTargetAmount(speed);
+    }
+}
---
+++ Assets/Scripts/Emhance/filledImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class filledImage : MonoBehaviour
+{
+    Image image;
+    float nowAmount = 0.5f;
+    float targetAmount = 0.5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = Mathf.Lerp(nowAmount, targetAmount, 0.8f);
+        nowAmount = Mathf.Lerp(nowAmount, targetAmount, 0.8f);
+    }
+
+    public void changeTargetAmount(float statasValue)
+    {
+        targetAmount = statasValue / 100;
+    }
+}
