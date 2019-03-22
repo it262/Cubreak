a8084349aad11f3d4a1f16b86d085393fe467ba6
it262 <sasura516@yahoo.co.jp>
2019-03-23 01:03:26 +0900

    Attack03

--- Assets/Scripts/Players/Attack.cs
+++ Assets/Scripts/Players/Attack.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-	[SerializeField]GameObject Arm,Axis;
+	[SerializeField]GameObject Arm;
 
-	public float speed = 1f;
+	public float preSpeed = -20f;
+	public Vector3 toScale = new Vector3(1f,1f,1f);
+	Vector3 preScale;
+
+	float speed;
+	float scale;
 
 	Vector3 buffer;
 
@@ -14,22 +19,26 @@ public class Attack : MonoBehaviour
     void Start()
     {
 		buffer = Arm.transform.localPosition;
+		speed = preSpeed;
+		preScale = Arm.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-		//if (Input.GetMouseButton (0)) {
-		if (Axis.transform.localPosition.z <= Arm.transform.position.z) {
-			Debug.Log ("Attack");
-			Arm.transform.RotateAround (Axis.transform.position, Vector3.up, -speed);
-			} else {
+		if (Input.GetMouseButton (0)) {
+			if (Vector3.Angle (buffer, Arm.transform.localPosition) >= 60) {
+				speed = preSpeed;
 				Arm.transform.localPosition = buffer;
+				Arm.transform.localScale = preScale;
+			} else {
+				Arm.transform.RotateAround (transform.localPosition, Vector3.up, speed=Mathf.Lerp(speed,-1,0.1f));
+				Arm.transform.localScale = Vector3.Lerp (Arm.transform.localScale, toScale, 0.5f);
 			}
-		/*
 		} else {
 			Arm.transform.localPosition = buffer;
+			Arm.transform.localScale = preScale;
+			speed = preSpeed;
 		}
-		*/
     }
 }
