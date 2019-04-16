8fcf7699956e71563dd6fe7fa85841df4d1584d9
it262 <sasura516@yahoo.co.jp>
2019-04-16 22:03:16 +0900

    ITaku20190416

--- Assets/Scripts/MainMenu/sphereController.cs
+++ Assets/Scripts/MainMenu/sphereController.cs
@@ -25,9 +25,12 @@ public class SphereController : MonoBehaviour {
             clickPosition.z = 10f;
             // オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
             // ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
-            Instantiate(Prefab,
-                Camera.main.ScreenToWorldPoint(clickPosition),
-                Prefab.transform.rotation);
+			if (transform.childCount < 20) {
+				GameObject g = (GameObject)Instantiate (Prefab,
+					              Camera.main.ScreenToWorldPoint (clickPosition),
+					              Prefab.transform.rotation);
+				g.transform.parent = this.gameObject.transform;
+			}
         }
     }
 }
